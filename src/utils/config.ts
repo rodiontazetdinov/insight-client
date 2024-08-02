55724faafdd4afe49b33dca00b75f80971091a8d
rodiontazetdinov <rodiontazetdinov@yandex.ru>
2024-08-02 10:25:45 +0300

    поменять на прод

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,7 @@
 export const TMA_URL = "https://t.me/why_invalid_bot/hackbets";
 export const APP_NAME = "Insight";
-export const IS_PRODUCTION = false;
-// export const IS_PRODUCTION = true;
+// export const IS_PRODUCTION = false;
+export const IS_PRODUCTION = true;
 export const CONTRACT_ADDRESS = "EQAnHTKm9s8X0_F1norY9efkETxuuDTtBQnzkgdQT15odaxW";
 export const MANIFEST_URL = "https://rodiontazetdinov.github.io/avatar-server/tonconnect-manifest.json";
 export const TELEMETRY_PRODUCT_ID = "c52c8a69-7e63-4bad-866c-749b1731a688";
