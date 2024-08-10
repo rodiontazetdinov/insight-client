160e2271980d649dffc6030c0889a985e27c60e6
rodiontazetdinov <rodiontazetdinov@yandex.ru>
2024-08-10 17:36:08 +0300

    подключить телеметрию

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,13 @@
-export const TMA_URL = "https://t.me/why_invalid_bot/hackbets";
-export const APP_NAME = "hackaTON Votes";
+export const TMA_URL = "https://t.me/insightmeta_bot/insight";
+
 // export const IS_PRODUCTION = false;
 export const IS_PRODUCTION = true;
-export const CONTRACT_ADDRESS = "EQAnHTKm9s8X0_F1norY9efkETxuuDTtBQnzkgdQT15odaxW";
-export const MANIFEST_URL = "https://rodiontazetdinov.github.io/avatar-server/tonconnect-manifest.json";
+export const CONTRACT_ADDRESS =
+  "EQAnHTKm9s8X0_F1norY9efkETxuuDTtBQnzkgdQT15odaxW";
+export const MANIFEST_URL =
+  "https://rodiontazetdinov.github.io/avatar-server/insight-tonconnect-manifest.json";
+
+//телеметрия https://app.telemetree.io/
+export const APP_NAME = "Insight";
+export const TLEMETREE_API_KEY = "b3a697fe-97ce-43d4-9c98-aa6deb1e72b9";
+export const TLEMETREE_PROJECT_ID = "c52c8a69-7e63-4bad-866c-749b1731a688";
